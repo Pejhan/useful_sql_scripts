20cd34869e9cc2f42e9b3ea02c42304837e65aaf
Pejhan <pejhan.red@gmail.com>
2025-12-09 13:50:58 +0330

    Update LogicalOrderOfMigration_Postgres.sql
    moved fks logic from subquery to temp table for both performance and brevity

--- LogicalOrderOfMigration_Postgres.sql
+++ LogicalOrderOfMigration_Postgres.sql
@@ -1,4 +1,20 @@
+drop table if exists fks;
 
+SELECT
+    tc.table_schema  AS source_schema,
+    tc.table_name    AS source_table,
+    ccu.table_schema AS target_schema,
+    ccu.table_name   AS target_table
+into temporary fks
+FROM information_schema.table_constraints AS tc
+JOIN information_schema.key_column_usage AS kcu
+    ON tc.constraint_name = kcu.constraint_name
+JOIN information_schema.constraint_column_usage AS ccu
+    ON ccu.constraint_name = tc.constraint_name
+WHERE 
+    tc.constraint_type = 'FOREIGN KEY' 
+    AND tc.table_schema = 'public'
+    AND tc.table_name != ccu.table_name;
     
     
 with recursive cte as (
@@ -9,39 +25,19 @@ with recursive cte as (
 		    '' AS target_table,
 		    1 as lvl
 	from information_schema.tables as t
-	left outer join (
-	    SELECT
-		    tc.table_schema  AS source_schema,
-		    tc.table_name    AS source_table,
-		    ccu.table_schema AS target_schema,
-		    ccu.table_name   AS target_table
-		FROM information_schema.table_constraints AS tc
-		JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
-		JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
-		WHERE tc.constraint_type = 'FOREIGN KEY' and tc.table_schema = 'public'
-	) a
-	on a.source_table = t.table_name
-	where a.source_table is null and t.table_schema = 'public' and t.table_type = 'BASE TABLE'
+	left outer join fks f
+	on f.source_table = t.table_name
+	where f.source_table is null and t.table_schema = 'public' and t.table_type = 'BASE TABLE'
 union all
 	select
-	    r.source_schema,
-	    r.source_table,
+	    f.source_schema,
+	    f.source_table,
 	    c.source_schema COLLATE "default" ::text,
 	    c.source_table COLLATE "default" ::text,
 	    c.lvl + 1 as lvl
 	from cte as c
-	inner join (
-		SELECT
-		    tc.table_schema  AS source_schema,
-		    tc.table_name    AS source_table,
-		    ccu.table_schema AS target_schema,
-		    ccu.table_name   AS target_table
-		FROM information_schema.table_constraints AS tc
-		JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
-		JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
-		WHERE tc.constraint_type = 'FOREIGN KEY' and tc.table_schema = 'public'
-	) r on r.target_table = c.source_table
-	where r.source_table != c.source_table
+	inner join fks f on f.target_table = c.source_table
+	where f.source_table != c.source_table
 )
 
 
