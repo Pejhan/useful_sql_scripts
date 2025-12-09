4f073fa5d7c08b9da5457f04ed19374a151cb675
Pejhan <pejhan.red@gmail.com>
2025-12-09 14:21:05 +0330

    indirect loop management
    To prevent an infinite loop which causes the script to run indefinitely

--- LogicalOrderOfMigration_Postgres.sql
+++ LogicalOrderOfMigration_Postgres.sql
@@ -15,34 +15,42 @@ WHERE
     tc.constraint_type = 'FOREIGN KEY' 
     AND tc.table_schema = 'public'
     AND tc.table_name != ccu.table_name;
-    
-    
-with recursive cte as (
-	select 
-		    t.table_schema   AS source_schema,
-		    t.table_name    AS source_table,
-		    '' AS target_schema,
-		    '' AS target_table,
-		    1 as lvl
-	from information_schema.tables as t
-	left outer join fks f
-	on f.source_table = t.table_name
-	where f.source_table is null and t.table_schema = 'public' and t.table_type = 'BASE TABLE'
-union all
-	select
-	    f.source_schema,
-	    f.source_table,
-	    c.source_schema COLLATE "default" ::text,
-	    c.source_table COLLATE "default" ::text,
-	    c.lvl + 1 as lvl
-	from cte as c
-	inner join fks f on f.target_table = c.source_table
-	where f.source_table != c.source_table
+
+
+WITH RECURSIVE cte AS (
+    SELECT 
+        t.table_schema AS source_schema,
+        t.table_name   AS source_table,
+        '' collate "default" :: text       AS target_schema,
+        '' collate "default" :: text       AS target_table,
+        1 AS lvl,
+        ARRAY[t.table_name] AS path
+    FROM information_schema.tables AS t
+    LEFT JOIN fks f ON f.source_table = t.table_name
+    WHERE 
+        f.source_table IS NULL
+        AND t.table_schema = 'public'
+        AND t.table_type = 'BASE TABLE'
+
+    UNION ALL
+
+    SELECT
+        f.source_schema,
+        f.source_table,
+        c.source_schema,
+        c.source_table,
+        c.lvl + 1 AS lvl,
+        c.path || f.source_table
+    FROM cte c
+    JOIN fks f ON f.target_table = c.source_table
+    WHERE f.source_table <> c.source_table --direct loop
+      AND f.source_table <> ALL (c.path) --indirect loop
 )
 
-
-select 
-	source_table, 
-	MAX(lvl) as orderNo 
-from cte
-group by source_table
+SELECT 
+	c.source_schema,
+	c.source_table,
+	MAX(c.lvl) as orderNo
+FROM cte as c
+group by c.source_schema, c.source_table
+order by 3 asc;
